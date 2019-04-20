c96ffd9a5b40b7a5b78033884a6ba7ec49c00e8d
wf <mytemp2@163.com>
2019-04-20 15:29:45 +0800

    create project, gen spider

---
+++ comics/comics/spiders/comic.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from selenium import webdriver
+from scrapy.selector import Selector
+from scrapy import Request
+import time
+
+
+class ComicSpider(scrapy.Spider):
+    name = 'comic'
+    allowed_domains = ['www.36mh.com']
+
+    def start_requests(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get('https://www.36mh.com/manhua/heijiao/70245.html')
+
+        sel = Selector(text=self.driver.page_source)
+        start_time = time.time()
+        while not sel.xpath('//*[@id="imgLoading"]/@style').extract():
+            time.sleep(1)
+            sel = Selector(text=self.driver.page_source)
+            if time.time() - start_time > 5:
+                raise Exception('Failed to get image ~ !')
+
+        print('----------------------------')
+        print(sel.xpath('//*[@id="imgLoading"]'))
+        print(sel.xpath('//*[@id="imgLoading"]/@style').extract())
+        print(sel.xpath('//*[@id="images"]/img/@src').extract())
+        print('----------------------------')
+
+        yield Request('https://www.36mh.com/manhua/heijiao/70245.html', callback=self.parse)
+
+    def parse(self, response):
+        pass
